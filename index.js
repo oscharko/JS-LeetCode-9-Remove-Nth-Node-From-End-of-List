e7bb24b55357200dbf66e70d5d2bfe1cfdf7f1bc
oscharko83@gmail.com <59687448+oscharko@users.noreply.github.com>
2022-05-31 10:23:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let head = [1,2,3,4,5], n = 2
+let head = [[1, 2, 3, 4, 5], [1], [1, 2]],
+    n = [2, 1, 1];
+    expects = [[1, 2, 3, 5], [], [1]];
 
-const removeNthFromEnd = function(head, n) {
-    return [head, n]
+const removeNthFromEnd = function (head, n, expects) {
+  expectsReturns = [];
+  for (let i = 0; i < head.length; i++) {
+    head[i].splice([head[i].length - n[i]], 1);
+    if (JSON.stringify(head[i]) === JSON.stringify(expects[i])) {
+      expectsReturns.push(true);
+    } else {
+      expectsReturns.push(false);
+    }
+  }
+  return expectsReturns;
 };
 
-console.log(removeNthFromEnd(head, n))
+console.log(removeNthFromEnd(head, n, expects));
